f647643c772d4c1e89ee209f837eb32a60bf6ce0
FAMASoon <famasoon@gmail.com>
2025-11-01 13:08:38 +0900

    Update CMake configuration to link additional LLVM components for enhanced optimization and transformation capabilities.

--- src/assembly_lifter.cpp
+++ src/assembly_lifter.cpp
@@ -3,6 +3,7 @@
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Intrinsics.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Pass.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Transforms/IPO.h>
 #include <llvm/Transforms/Scalar.h>
